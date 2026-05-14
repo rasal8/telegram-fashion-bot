73cfaad7828788ac9349d1683f42576d5a9d27db
rasal8 <mobileaccessories122@gmail.com>
2026-05-14 14:21:41 +0530

    Refactor bot.py to simplify main function

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
@@ -28,15 +27,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         await update.message.reply_text("Product not found.")
 
-async def main():
+app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
 
-    app.add_handler(CommandHandler("start", start))
+print("Bot Running...")
 
-    print("Bot Running...")
-
-    await app.run_polling()
-
-if __name__ == "__main__":
-    asyncio.run(main())
+app.run_polling(close_loop=False)
